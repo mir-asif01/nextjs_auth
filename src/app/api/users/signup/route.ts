092b41b8e39b1fd529af7344529cfa07252a102b
mir-asif01 <asifbsb2002@gmail.com>
2024-10-13 10:12:08 +0600

    signup route added

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,1 +1,37 @@
+import { connectDb } from "@/db/connectDb"
+import User from "@/models/userModel"
+import { NextRequest, NextResponse } from "next/server"
+import bcrypt from "bcryptjs"
+import { sendMail } from "@/utils/mailer"
 
+connectDb()
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = request.json()
+    const { username, email, password }: any = reqBody
+    const user = await User.findOne({ email })
+    if (user) {
+      return NextResponse.json({ message: "User already exists. try login" })
+    }
+    const salt = await bcrypt.genSalt(9)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    })
+    const savedUser = await newUser.save()
+    // send email
+    await sendMail({ email, emailType: "VERIFY", userId: savedUser._id })
+
+    return NextResponse.json({
+      message: "User signup successful",
+      success: true,
+      savedUser,
+    })
+  } catch (error: any) {
+    return NextResponse.json({ error: error?.message })
+  }
+}
